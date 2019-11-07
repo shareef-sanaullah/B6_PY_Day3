9e7da4772de8e9c4658889f6b3f9db3d537a3bad
shareef-sanaullah <57405273+shareef-sanaullah@users.noreply.github.com>
2019-11-07 21:44:30 +0300

    Add files via upload

---
+++ Batch6_Python_Day3.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+2+5
+
+
+# In[2]:
+
+
+4-1
+
+
+# In[3]:
+
+
+5*7
+
+
+# In[4]:
+
+
+4**7
+
+
+# In[8]:
+
+
+4***3
+
+
+# In[9]:
+
+
+1.0+4.5
+
+
+# In[10]:
+
+
+x = 2.8
+y = 4.2
+
+z = x+y
+
+print(z)
+
+
+# In[11]:
+
+
+type(z)
+
+
+# In[12]:
+
+
+x = 2.5
+y = 4.5
+
+z = x+y
+
+
+# In[15]:
+
+
+print(z)
+
+
+# In[14]:
+
+
+type(z)
+
+
+# In[16]:
+
+
+x = 30
+y = 45
+
+z =x+y
+
+print(z)
+
+
+# In[17]:
+
+
+type(z)
+
+
+# In[18]:
+
+
+students = ['naveed','abubacker','abdulla','vikrant','rafeeq']
+
+print(students)
+
+
+# In[19]:
+
+
+type(students)
+
+
+# In[20]:
+
+
+students1 = ['waseem','talha','javeed','rohid','shareef']
+
+print(students1)
+
+
+# In[21]:
+
+
+print(students1 [3])
+
+
+# In[22]:
+
+
+print(students1 [1].title())
+
+
+# In[23]:
+
+
+students2 = [' khaled','kranthi ','vasil','sarfaraz']
+
+print(students2 [0].lstrip())
+
+
+# In[24]:
+
+
+print(students2[1].rstrip().title())
+
+
+# In[25]:
+
+
+message = f"you are doing a good job,\n{students2[2].upper()}"
+
+
+# In[26]:
+
+
+print(message)
+
+
+# In[ ]:
+
+
+
+
